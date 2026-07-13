5d27cad3066901265ad8a4a952495160cf584d16
spiralbot <ci-bot@spiralscout.com>
2026-07-13 14:32:23 +0000

    Pass EventDispatcher into \Spiral\Http\Http on creating (#1292)

--- tests/Stub/TestAuthToken.php
+++ tests/Stub/TestAuthToken.php
@@ -17,37 +17,23 @@ class TestAuthToken implements TokenInterface
     /** @var array */
     private $payload;
 
-    /**
-     * @param string                  $id
-     * @param array                   $payload
-     * @param \DateTimeInterface|null $expiresAt
-     */
-    public function __construct(string $id, array $payload, \DateTimeInterface $expiresAt = null)
+    public function __construct(string $id, array $payload, ?\DateTimeInterface $expiresAt = null)
     {
         $this->id = $id;
         $this->expiresAt = $expiresAt;
         $this->payload = $payload;
     }
 
-    /**
-     * @inheritDoc
-     */
     public function getID(): string
     {
         return $this->id;
     }
 
-    /**
-     * @inheritDoc
-     */
     public function getExpiresAt(): ?\DateTimeInterface
     {
         return $this->expiresAt;
     }
 
-    /**
-     * @inheritDoc
-     */
     public function getPayload(): array
     {
         return $this->payload;
